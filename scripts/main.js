6d0f9f3fc8436c0370247984fd7c6eb50b09fe0b
charmerDark <49775559+charmerDark@users.noreply.github.com>
2019-06-01 21:09:19 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/travel.jpg') {
+      	myImage.setAttribute ('src','images/travel2.jpg');
+    	} 
+    else {
+      	myImage.setAttribute ('src','images/travel.jpg');
+    	}
+    }
+var myHeading=document.querySelector('h2');
+var myButton=document.querySelector('button');
+function setUserName(){
+	var myName=prompt('Enter Your Name Seeker');
+	localStorage.setItem('name',myName);
+	myHeading.textContent='It\'s your chance to lose yourself '+myName;
+}
+if(!localStorage.getItem('name')){
+	setUserName();
+}
+else{
+	var storedName=localStorage.getItem('name');
+	myHeading.textContent='It\'s your chance to lose yourself '+storedName;
+}
+myButton.onclick=function(){
+	setUserName();
+}
+console.log(showCookies());
---
+++ styles/main.js
@@ -0,0 +1,2 @@
+var myHeading=queryselector('h2');
+myHeading.textContent='Hello Crazy Place';
